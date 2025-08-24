d49ab9174cabb1944eb403b41e43153717ac03cf
victoriairiarte <91188343+victoriairiarte@users.noreply.github.com>
2025-08-24 13:15:35 -0300

    Update analysis_pipeline.R

--- R/analysis_pipeline.R
+++ R/analysis_pipeline.R
@@ -397,4 +397,512 @@ betas1<-table1[,c(1,2,5)]
 write.csv(table_1, "ve_1week.csv")
 table_1$VE1
 
- 
+ #WEEK 2----
+
+##Zone pre----
+
+# Step 1: Define the threshold date for creating the dummy variable for vaccination
+threshold_date <- as.Date("2001-05-18")
+threshold_date2<-as.Date("2001-05-11")
+
+# Step 2: Create a sequence of time steps (daily)
+start_date <- min(pre$virus_intro, na.rm = TRUE)
+end_date <- max(pre$virus_intro, na.rm = TRUE)
+time_steps <- seq(from = start_date, to = end_date, by = "day")
+
+# Step 3: Create a data frame for storing results
+results <- data.frame(date = time_steps)
+
+# Step 4: Calculate cases, infectious, and susceptible farms, and determine vaccination status for each time step
+prepared_data <- results %>%
+  rowwise() %>%
+  mutate(
+    # Calculate the number of cases at the current date
+    cases = sum(pre$virus_intro == date, na.rm = TRUE),
+    
+    # Calculate the number of infectious farms (those still within their infectious period of 26 days)
+    infectious = sum(pre$virus_intro <= date & 
+                       (pre$virus_intro + 26) >= date, na.rm = TRUE),
+    
+    recovered= sum((pre$virus_intro + 26) < date, na.rm=TRUE),
+    
+    # Calculate the number of susceptible farms
+    susceptible = sum(is.na(pre$virus_intro) | 
+                        pre$virus_intro > date, na.rm = TRUE),
+    N= susceptible+infectious+recovered,
+    
+    # Determine vaccination status based on the current date relative to the threshold date
+    vaccination = ifelse(date < threshold_date, 0, 1),
+    interval= ifelse(date < threshold_date2, 0, 1),
+    
+    # Calculate deltaT (time difference in days between current and previous step)
+    deltaT = ifelse(date == lag(date), 1, as.numeric(difftime(date, lag(date), units = "days")))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    deltaT = ifelse(is.na(deltaT), 1, deltaT),
+    
+    # Calculate the offset as log(infectious / susceptible * deltaT), ensuring there are no zero values
+    offset = log(ifelse(susceptible > 0, infectious / N * deltaT, 1))
+  )
+
+
+# Step 5: Fit the GLM including vaccination as a predictor
+glm_model_vac <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                     offset = offset, 
+                     family = binomial(link = "cloglog"), 
+                     data = prepared_data)
+
+vac_beta <- glm(cbind(cases, susceptible - cases) ~ 0+vaccination, 
+                offset = offset, 
+                family = binomial(link = "cloglog"), 
+                data = prepared_data)
+
+# interval = 1 from the vaccination date (11/05), marking herds as vaccinated
+# vaccination = 1 one week later (18/05), when protection is assumed effective
+# → this allows excluding the “grey window” (11–18/05), when animals were vaccinated but not yet fully protected
+
+subset_data <- prepared_data %>% 
+  filter((vaccination == 0 & interval == 0) | 
+           (vaccination == 1 & interval == 1))
+
+model.pre <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                 offset = offset, 
+                 family = binomial(link = "cloglog"), 
+                 data = subset_data)
+
+tidy.pre_two<-tidy(model.pre,exponentiate = T, conf.int = T)
+tidy.pre2<-tidy(model.pre,exponentiate = T, conf.int = T)
+tidy.pre2$zone<-"pre"
+tidy.pre2<-tidy.pre2[2,c(1,2,6,7,8)]
+tidy.pre2$VE<-(1-tidy.pre2$estimate)*100
+tidy.pre2$VE.lower<-(1-tidy.pre2$conf.high)*100
+tidy.pre2$VE.upper<-(1-tidy.pre2$conf.low)*100
+
+tidy.pre2<-tidy.pre2[,c("zone" ,"VE", "VE.lower", "VE.upper")]
+###VE pre----
+tidy.pre2
+
+summary(vac_beta)
+r2_pre<-tidy(vac_beta,exponentiate = T, conf.int = T)
+r2_pre$zone<-"pre"
+
+# Display the summary of the model
+summary(glm_model_vac)
+r2_pre2<-tidy(glm_model_vac,exponentiate = T, conf.int = T)
+r2_pre2$zone<-"pre"
+relative_risk2 <- exp(coef(glm_model_vac)[2])
+(1-relative_risk2)*100
+
+##Zone A----
+
+A<- prueba3%>%
+  filter(Zone=="A")
+
+# Step 1: Define the threshold date for creating the dummy variable for vaccination
+threshold_date <- as.Date("2001-05-19")
+threshold_date2<-as.Date("2001-05-12")
+
+# Step 2: Create a sequence of time steps (daily)
+start_date <- min(A$virus_intro, na.rm = TRUE)
+end_date <- max(A$virus_intro, na.rm = TRUE)
+time_steps <- seq(from = start_date, to = end_date, by = "day")
+
+# Step 3: Create a data frame for storing results
+results <- data.frame(date = time_steps)
+
+# Step 4: Calculate cases, infectious, and susceptible farms, and determine vaccination status for each time step
+prepared_data <- results %>%
+  rowwise() %>%
+  mutate(
+    # Calculate the number of cases at the current date
+    cases = sum(A$virus_intro == date, na.rm = TRUE),
+    
+    # Calculate the number of infectious farms (those still within their infectious period of 26 days)
+    infectious = sum(A$virus_intro <= date & 
+                       (A$virus_intro + 26) >= date, na.rm = TRUE),
+    
+    recovered= sum((A$virus_intro + 26) < date, na.rm=TRUE),
+    
+    # Calculate the number of susceptible farms
+    susceptible = sum(is.na(A$virus_intro) | 
+                        A$virus_intro > date, na.rm = TRUE),
+    N= susceptible+infectious+recovered,
+    
+    # Determine vaccination status based on the current date relative to the threshold date
+    vaccination = ifelse(date < threshold_date, 0, 1),
+    interval= ifelse(date < threshold_date2, 0, 1),
+    
+    # Calculate deltaT (time difference in days between current and previous step)
+    deltaT = ifelse(date == lag(date), 1, as.numeric(difftime(date, lag(date), units = "days")))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    deltaT = ifelse(is.na(deltaT), 1, deltaT),
+    
+    # Calculate the offset as log(infectious / susceptible * deltaT), ensuring there are no zero values
+    offset = log(ifelse(susceptible > 0, infectious / N * deltaT, 1))
+  )
+
+
+# Step 5: Fit the GLM including vaccination as a predictor
+glm_model_vac <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                     offset = offset, 
+                     family = binomial(link = "cloglog"), 
+                     data = prepared_data)
+
+vac_beta <- glm(cbind(cases, susceptible - cases) ~ 0+vaccination, 
+                offset = offset, 
+                family = binomial(link = "cloglog"), 
+                data = prepared_data)
+
+subset_data <- prepared_data %>% 
+  filter((vaccination == 0 & interval == 0) | 
+           (vaccination == 1 & interval == 1))
+model.A <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+               offset = offset, 
+               family = binomial(link = "cloglog"), 
+               data = subset_data)
+tidy.Atwo<-tidy(model.A,exponentiate = T, conf.int = T)
+tidy.A2<-tidy(model.A,exponentiate = T, conf.int = T)
+tidy.A2$zone<-"A"
+tidy.A2<-tidy.A2[2,c(1,2,6,7,8)]
+tidy.A2$VE<-(1-tidy.A2$estimate)*100
+tidy.A2$VE.lower<-(1-tidy.A2$conf.high)*100
+tidy.A2$VE.upper<-(1-tidy.A2$conf.low)*100
+
+tidy.A2<-tidy.A2[,c("zone" ,"VE", "VE.lower", "VE.upper")]
+###VE A----
+tidy.A2
+
+summary(vac_beta)
+r2_A<-tidy(vac_beta,exponentiate = T, conf.int = T)
+r2_A$zone<-"A"
+
+# Display the summary of the model
+summary(glm_model_vac)
+r2_A2<-tidy(glm_model_vac,exponentiate = T, conf.int = T)
+r2_A2$zone<-"A"
+relative_risk2 <- exp(coef(glm_model_vac)[2])
+(1-relative_risk2)*100
+
+##Zone B----
+
+B<- prueba3%>%
+  filter(Zone=="B")
+
+# Step 1: Define the threshold date for creating the dummy variable for vaccination
+threshold_date <- as.Date("2001-05-24")
+threshold_date2<-as.Date("2001-05-17")
+
+# Step 2: Create a sequence of time steps (daily)
+start_date <- min(B$virus_intro, na.rm = TRUE)
+end_date <- max(B$virus_intro, na.rm = TRUE)
+time_steps <- seq(from = start_date, to = end_date, by = "day")
+
+# Step 3: Create a data frame for storing results
+results <- data.frame(date = time_steps)
+
+# Step 4: Calculate cases, infectious, and susceptible farms, and determine vaccination status for each time step
+prepared_data <- results %>%
+  rowwise() %>%
+  mutate(
+    # Calculate the number of cases at the current date
+    cases = sum(B$virus_intro == date, na.rm = TRUE),
+    
+    # Calculate the number of infectious farms (those still within their infectious period of 26 days)
+    infectious = sum(B$virus_intro <= date & 
+                       (B$virus_intro + 26) >= date, na.rm = TRUE),
+    
+    recovered= sum((B$virus_intro + 26) < date, na.rm=TRUE),
+    
+    # Calculate the number of susceptible farms
+    susceptible = sum(is.na(B$virus_intro) | 
+                        B$virus_intro > date, na.rm = TRUE),
+    N= susceptible+infectious+recovered,
+    
+    # Determine vaccination status based on the current date relative to the threshold date
+    vaccination = ifelse(date < threshold_date, 0, 1),
+    interval= ifelse(date < threshold_date2, 0, 1),
+    
+    # Calculate deltaT (time difference in days between current and previous step)
+    deltaT = ifelse(date == lag(date), 1, as.numeric(difftime(date, lag(date), units = "days")))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    deltaT = ifelse(is.na(deltaT), 1, deltaT),
+    
+    # Calculate the offset as log(infectious / susceptible * deltaT), ensuring there are no zero values
+    offset = log(ifelse(susceptible > 0, infectious / N * deltaT, 1))
+  )
+
+
+# Step 5: Fit the GLM including vaccination as a predictor
+glm_model_vac <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                     offset = offset, 
+                     family = binomial(link = "cloglog"), 
+                     data = prepared_data)
+
+vac_beta <- glm(cbind(cases, susceptible - cases) ~ 0+vaccination, 
+                offset = offset, 
+                family = binomial(link = "cloglog"), 
+                data = prepared_data)
+
+subset_data <- prepared_data %>% 
+  filter((vaccination == 0 & interval == 0) | 
+           (vaccination == 1 & interval == 1))
+model.B <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+               offset = offset, 
+               family = binomial(link = "cloglog"), 
+               data = subset_data)
+
+tidy.Btwo<-tidy(model.B,exponentiate = T, conf.int = T)
+tidy.B2<-tidy(model.B,exponentiate = T, conf.int = T)
+tidy.B2$zone<-"B"
+tidy.B2<-tidy.B2[2,c(1,2,6,7,8)]
+tidy.B2$VE<-(1-tidy.B2$estimate)*100
+tidy.B2$VE.lower<-(1-tidy.B2$conf.high)*100
+tidy.B2$VE.upper<-(1-tidy.B2$conf.low)*100
+
+tidy.B2<-tidy.B2[,c("zone" ,"VE", "VE.lower", "VE.upper")]
+###VE B----
+tidy.B2
+
+summary(vac_beta)
+r2_B<-tidy(vac_beta,exponentiate = T, conf.int = T)
+r2_B$zone<-"B"
+# Display the summary of the model
+summary(glm_model_vac)
+r2_B2<-tidy(glm_model_vac,exponentiate = T, conf.int = T)
+r2_B2$zone<-"B"
+relative_risk2 <- exp(coef(glm_model_vac)[2])
+(1-relative_risk2)*100
+
+##Zone C----
+
+C<- prueba3%>%
+  filter(Zone=="C")
+
+# Step 1: Define the threshold date for creating the dummy variable for vaccination
+threshold_date <- as.Date("2001-05-28")
+threshold_date2<-as.Date("2001-05-22")
+
+# Step 2: Create a sequence of time steps (daily)
+start_date <- min(C$virus_intro, na.rm = TRUE)
+end_date <- max(C$virus_intro, na.rm = TRUE)
+time_steps <- seq(from = start_date, to = end_date, by = "day")
+
+# Step 3: Create a data frame for storing results
+results <- data.frame(date = time_steps)
+
+# Step 4: Calculate cases, infectious, and susceptible farms, and determine vaccination status for each time step
+prepared_data <- results %>%
+  rowwise() %>%
+  mutate(
+    # Calculate the number of cases at the current date
+    cases = sum(C$virus_intro == date, na.rm = TRUE),
+    
+    # Calculate the number of infectious farms (those still within their infectious period of 26 days)
+    infectious = sum(C$virus_intro <= date & 
+                       (C$virus_intro + 26) >= date, na.rm = TRUE),
+    
+    recovered= sum((C$virus_intro + 26) < date, na.rm=TRUE),
+    
+    # Calculate the number of susceptible farms
+    susceptible = sum(is.na(C$virus_intro) | 
+                        C$virus_intro > date, na.rm = TRUE),
+    N= susceptible+infectious+recovered,
+    
+    # Determine vaccination status based on the current date relative to the threshold date
+    vaccination = ifelse(date < threshold_date, 0, 1),
+    interval= ifelse(date < threshold_date2, 0, 1),
+    
+    # Calculate deltaT (time difference in days between current and previous step)
+    deltaT = ifelse(date == lag(date), 1, as.numeric(difftime(date, lag(date), units = "days")))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    deltaT = ifelse(is.na(deltaT), 1, deltaT),
+    
+    # Calculate the offset as log(infectious / susceptible * deltaT), ensuring there are no zero values
+    offset = log(ifelse(susceptible > 0, infectious / N * deltaT, 1))
+  )
+
+
+# Step 5: Fit the GLM including vaccination as a predictor
+glm_model_vac <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                     offset = offset, 
+                     family = binomial(link = "cloglog"), 
+                     data = prepared_data)
+
+vac_beta <- glm(cbind(cases, susceptible - cases) ~ 0+vaccination, 
+                offset = offset, 
+                family = binomial(link = "cloglog"), 
+                data = prepared_data)
+
+subset_data <- prepared_data %>% 
+  filter((vaccination == 0 & interval == 0) | 
+           (vaccination == 1 & interval == 1))
+model.C <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+               offset = offset, 
+               family = binomial(link = "cloglog"), 
+               data = subset_data)
+tidy.Ctwo<-tidy(model.C,exponentiate = T, conf.int = T)
+tidy.C2<-tidy(model.C,exponentiate = T, conf.int = T)
+tidy.C2$zone<-"C"
+tidy.C2<-tidy.C2[2,c(1,2,6,7,8)]
+tidy.C2$VE<-(1-tidy.C2$estimate)*100
+tidy.C2$VE.lower<-(1-tidy.C2$conf.high)*100
+tidy.C2$VE.upper<-(1-tidy.C2$conf.low)*100
+
+tidy.C2<-tidy.C2[,c("zone" ,"VE", "VE.lower", "VE.upper")]
+###VE C----
+tidy.C2
+
+summary(vac_beta)
+r2_C<-tidy(vac_beta,exponentiate = T, conf.int = T)
+r2_C$zone<-"C"
+
+# Display the summary of the model
+summary(glm_model_vac)
+r2_C2<-tidy(glm_model_vac,exponentiate = T, conf.int = T)
+r2_C2$zone<-"C"
+relative_risk2 <- exp(coef(glm_model_vac)[2])
+(1-relative_risk2)*100
+
+##Zone D----
+
+D<- prueba3%>%
+  filter(Zone=="D")
+
+# Step 1: Define the threshold date for creating the dummy variable for vaccination
+threshold_date <- as.Date("2001-06-04")
+threshold_date2<-as.Date("2001-05-28")
+
+# Step 2: Create a sequence of time steps (daily)
+start_date <- min(D$virus_intro, na.rm = TRUE)
+end_date <- max(D$virus_intro, na.rm = TRUE)
+time_steps <- seq(from = start_date, to = end_date, by = "day")
+
+# Step 3: Create a data frame for storing results
+results <- data.frame(date = time_steps)
+
+# Step 4: Calculate cases, infectious, and susceptible farms, and determine vaccination status for each time step
+prepared_data <- results %>%
+  rowwise() %>%
+  mutate(
+    # Calculate the number of cases at the current date
+    cases = sum(D$virus_intro == date, na.rm = TRUE),
+    
+    # Calculate the number of infectious farms (those still within their infectious period of 26 days)
+    infectious = sum(D$virus_intro <= date & 
+                       (D$virus_intro + 26) >= date, na.rm = TRUE),
+    
+    recovered= sum((D$virus_intro + 26) < date, na.rm=TRUE),
+    
+    # Calculate the number of susceptible farms
+    susceptible = sum(is.na(D$virus_intro) | 
+                        D$virus_intro > date, na.rm = TRUE),
+    N= susceptible+infectious+recovered,
+    
+    # Determine vaccination status based on the current date relative to the threshold date
+    vaccination = ifelse(date < threshold_date, 0, 1),
+    interval= ifelse(date < threshold_date2, 0, 1),
+    
+    # Calculate deltaT (time difference in days between current and previous step)
+    deltaT = ifelse(date == lag(date), 1, as.numeric(difftime(date, lag(date), units = "days")))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    deltaT = ifelse(is.na(deltaT), 1, deltaT),
+    
+    # Calculate the offset as log(infectious / susceptible * deltaT), ensuring there are no zero values
+    offset = log(ifelse(susceptible > 0, infectious / N * deltaT, 1))
+  )
+
+
+# Step 5: Fit the GLM including vaccination as a predictor
+glm_model_vac <- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+                     offset = offset, 
+                     family = binomial(link = "cloglog"), 
+                     data = prepared_data)
+
+vac_beta <- glm(cbind(cases, susceptible - cases) ~ 0+vaccination, 
+                offset = offset, 
+                family = binomial(link = "cloglog"), 
+                data = prepared_data)
+
+subset_data <- prepared_data %>% 
+  filter((vaccination == 0 & interval == 0) | 
+           (vaccination == 1 & interval == 1))
+model.D<- glm(cbind(cases, susceptible - cases) ~ vaccination, 
+              offset = offset, 
+              family = binomial(link = "cloglog"), 
+              data = subset_data)
+
+tidy.Dtwo<-tidy(model.D,exponentiate = T, conf.int = T)
+
+tidy.D2<-tidy(model.D,exponentiate = T, conf.int = T)
+tidy.D2$zone<-"D"
+tidy.D2<-tidy.D2[2,c(1,2,6,7,8)]
+tidy.D2$VE<-(1-tidy.D2$estimate)*100
+tidy.D2$VE.lower<-(1-tidy.D2$conf.high)*100
+tidy.D2$VE.upper<-(1-tidy.D2$conf.low)*100
+
+tidy.D2<-tidy.D2[,c("zone" ,"VE", "VE.lower", "VE.upper")]
+###VE D----
+tidy.D2
+
+####Table VE week 2----
+VE_week2<-rbind(tidy.pre2,tidy.A2,tidy.B2,tidy.C2, tidy.D2)
+write.csv(VE_week2, "VE_week2.csv")
+
+summary(vac_beta)
+r2_D<-tidy(vac_beta,exponentiate = T, conf.int = T)
+r2_D$zone<-"D"
+
+# Display the summary of the model
+summary(glm_model_vac)
+r2_D2<-tidy(glm_model_vac,exponentiate = T, conf.int = T)
+r2_D2$zone<-"D"
+
+relative_risk2 <- exp(coef(glm_model_vac)[2])
+(1-relative_risk2)*100
+
+#Final table Week 2----
+
+bvac2<-rbind(r2_pre,r2_A, r2_B, r2_C,r2_D)
+bvac2<-bvac2[,c("zone" ,"estimate", "conf.low", "conf.high")]
+colnames(bvac2)<-c("zone" ,"beta_vac2", "conf.low", "conf.high")
+
+bunvac_rel2<-rbind(r2_pre2,r2_A2, r2_B2, r2_C2,r2_D2)
+
+brel2<-bunvac_rel2%>%
+  filter(term=="vaccination")
+
+brel2<-brel2[,c("zone" ,"estimate", "conf.low", "conf.high")]
+colnames(brel2)<-c("zone" ,"beta_rel2", "conf.low", "conf.high")
+brel2$VE<-(1-brel2$beta_rel2)*100
+brel2$VE.low<-(1-brel2$conf.high)*100
+brel2$VE.high<-(1-brel2$conf.low)*100
+brel2<-brel2[,c("zone", "VE", "VE.low", "VE.high")]
+#merged_df <- left_join(df1, df2, by = "key_column")
+table2<-left_join(bvac2,brel2, by="zone")
+
+table2$beta_vac2<- sprintf("%.4f [%.4f - %.4f]", 
+                           table2$beta_vac2, table2$conf.low, 
+                           table2$conf.high)
+table2$VE2<- sprintf("%.2f [%.2f - %.2f]", 
+                     table2$VE, table2$VE.low, 
+                     table2$VE.high)
+table_2<-table2[,c("zone", "beta_vac2", "VE2")]
+
+write.csv(table_2, "ve_2week.csv")
+###beta 2 week----
+beta2<- table2[,c(1,2)]
+
+#Outputs Betas----
+
+outputs_2<-rbind(tidy.pre_two, tidy.Atwo, tidy.Btwo, tidy.Ctwo,
+                 tidy.Dtwo)
+
